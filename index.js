a797adacf039e7583e3a8ca9c5e5f9be32379417
Odair Carvalho <odaircarvalho23@gmail.com>
2020-04-01 21:51:58 -0300

    Finalização do Modulo

--- index.js
+++ index.js
@@ -2,8 +2,52 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req, res )=>{
-    return res.json({message: 'Olá mundoooo!'});
+server.use(express.json());
+
+const users = ['Claudio','Odair','Laura', 'Lucia', 'Joana Dark'];
+
+
+//Middleware GLobal
+server.use((req, res, next)=>{
+    console.log(`metodo ${req.method}; URL ${req.url}`);
+ return next();
 });
 
-server.listen(8002);
+//Middleware Local( ppode ser chamado dentro dos metodos)
+function chekUsers(req, res, next){
+    if(!req.body.name){
+        return res.status(400).json({error: 'User name is requered'});   
+    }
+     return next();
+};
+function chekUsersinArray(req, res, next){
+    if(!users[req.params.index]){
+        return res.status(400).json({error: 'User does not exiist'});   
+    }
+     return next();
+};
+
+server.get('/users', (req, res )=>{
+    return res.json(users);
+});
+
+
+server.post('/users', chekUsers, (req, res)=>{
+    const {name} = req.body;
+    users.push(name);
+    return res.json(users);
+});
+server.put('/users/:index', chekUsers, chekUsersinArray, (req, res)=>{
+    const {index} = req.params;
+    const {name} = req.body;
+    users[index] = name ;
+    return res.json(users);
+});
+
+server.delete('/users/:index', chekUsersinArray, (req, res)=>{
+    const {index} = req.params;
+    users.splice(index,1);
+    return res.send("Usuário excluido com sucesso!");
+});
+
+server.listen(8001);
